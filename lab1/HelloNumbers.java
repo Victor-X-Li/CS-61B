caa4055cc47921197aacb8bea3b0b20d781aeaea
Victor-X-Li <Victor.X.Li@outlook.com>
2026-09-23 11:06:37 +0800

    fixed HelloNumbers

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,7 +1,7 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 0; i < 10; i++) {
             sum += i;
             System.out.println(sum);
         }
