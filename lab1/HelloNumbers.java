2274b14ce03c1688d2a45464f079dc0ad2e7f25c
Victor-X-Li <Victor.X.Li@outlook.com>
2026-09-23 11:02:28 +0800

    done with HelloWorld and HelloNumbers

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,11 +1,9 @@
 public class HelloNumbers {
     public static void main(String[] args) {
-        int x = 1;
-        int total = 0;
-        while (x <= 10) {
-            System.out.print(total + " ");
-            total = total + x;
-            x = x + 1;
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+            System.out.println(sum);
         }
 	}
 } 
